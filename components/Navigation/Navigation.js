584fb052b0c684ab7e6aafbbe52d41bf982ed774
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-04-11 10:53:38 +0200

    Added navigation hide on scrolling down and show on scrolling up

--- components/Navigation/Navigation.js
+++ components/Navigation/Navigation.js
@@ -1,6 +1,6 @@
 import { AnimatePresence, motion } from "framer-motion";
 import Link from "next/link";
-import React from "react";
+import { React, useState, useEffect } from "react";
 import SkipContentLink from "../SkipContentLink/SkipContentLink";
 import SkipToMain from "../SkipToMain/SkipToMain";
 import CloseButton from "./Buttons/CloseButton";
@@ -9,23 +9,53 @@ import SearchButton from "./Buttons/SearchButton";
 import Tag from "./Tag/Tag";
 
 export default function Navigation() {
-  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const toogleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
   };
 
-  const [isSearchOpen, setIsSearchOpen] = React.useState(false);
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
 
   const toogleSearch = () => {
     setIsSearchOpen(!isSearchOpen);
   };
 
-  const [isCategoriesOpen, setIsCategoriesOpen] = React.useState(false);
+  const [isCategoriesOpen, setIsCategoriesOpen] = useState(false);
 
   const toogleCategories = () => {
     setIsCategoriesOpen(!isCategoriesOpen);
   };
+
+  /* Hide navigation on scroll */
+  const [showNavbar, setshowNavbar] = useState(true);
+  const [lastScrollY, setLastScrollY] = useState(0);
+
+  const controlNavbar = () => {
+    if (typeof window !== "undefined") {
+      if (window.scrollY > lastScrollY) {
+        // if scroll down hide the navbar
+        setshowNavbar(false);
+      } else {
+        // if scroll up showNavbar the navbar
+        setshowNavbar(true);
+      }
+
+      // remember current page location to use in the next move
+      setLastScrollY(window.scrollY);
+    }
+  };
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      window.addEventListener("scroll", controlNavbar);
+
+      // cleanup function
+      return () => {
+        window.removeEventListener("scroll", controlNavbar);
+      };
+    }
+  }, [lastScrollY]);
 
   //Animations
   const dropIn = {
@@ -51,57 +81,62 @@ export default function Navigation() {
   return (
     <>
       <SkipContentLink />
-      <nav className="bg-primary text-white sticky top-0 z-10">
-        <div className="flex justify-between items-center p-4">
-          <Link href="/">
-            <a className="font-semibold text-xl">KV.blog</a>
-          </Link>
-          <div className="hidden space-x-6 md:flex items-center">
-            <SearchButton onclick={toogleSearch} />
+      {showNavbar && (
+        <motion.nav
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          className="bg-primary text-white sticky top-0 z-10"
+        >
+          <div className="flex justify-between items-center p-4">
             <Link href="/">
-              <a className="font-semibold text-xl link">Home</a>
+              <a className="font-semibold text-xl">KV.blog</a>
             </Link>
-            <button
-              className="font-semibold text-xl link"
-              onClick={toogleCategories}
-            >
-              Categories
-            </button>
-            <Link href="/">
-              <a className="font-semibold text-xl link">Contact me</a>
-            </Link>
-          </div>
+            <div className="hidden space-x-6 md:flex items-center">
+              <SearchButton onclick={toogleSearch} />
+              <Link href="/">
+                <a className="font-semibold text-xl link">Home</a>
+              </Link>
+              <button
+                className="font-semibold text-xl link"
+                onClick={toogleCategories}
+              >
+                Categories
+              </button>
+              <Link href="/">
+                <a className="font-semibold text-xl link">Contact me</a>
+              </Link>
+            </div>
 
-          <div className="flex space-x-4 md:hidden">
-            <SearchButton onclick={toogleSearch} />
-            <MenuButton onclick={toogleMenu} />
+            <div className="flex space-x-4 md:hidden">
+              <SearchButton onclick={toogleSearch} />
+              <MenuButton onclick={toogleMenu} />
+            </div>
           </div>
-        </div>
-        {/* Categories menu - DESKTOP ONLY */}
-        {isCategoriesOpen && (
-          <div className="hidden md:block bg-special p-4">
-            <ul className="text-center flex space-x-4 text-xl">
-              <li>
-                <Link href="/">
-                  <a className="link">Web development</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <a className="link">Frontend</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/">
-                  <a className="link">Backend</a>
-                </Link>
-              </li>
-            </ul>
-          </div>
-        )}
-        {/* Menu  - MOBILE ONLY */}
-        {isMenuOpen && (
-          <AnimatePresence>
+          {/* Categories menu - DESKTOP ONLY */}
+          {isCategoriesOpen && (
+            <div className="hidden md:block bg-special p-4">
+              <ul className="text-center flex space-x-4 text-xl">
+                <li>
+                  <Link href="/">
+                    <a className="link">Web development</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="link">Frontend</a>
+                  </Link>
+                </li>
+                <li>
+                  <Link href="/">
+                    <a className="link">Backend</a>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+          )}
+          {/* Menu  - MOBILE ONLY */}
+          {isMenuOpen && (
             <motion.div
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
@@ -126,52 +161,41 @@ export default function Navigation() {
                 </li>
               </ul>
             </motion.div>
-          </AnimatePresence>
-        )}
-      </nav>
+          )}
+        </motion.nav>
+      )}
+
       {/* Search */}
       {isSearchOpen && (
-        <AnimatePresence
-          // Disable any initial animations on children that
-          // are present when the component is first rendered
-          initial={true}
-          // Only render one component at a time.
-          // The exiting component will finish its exit
-          // animation before entering component is rendered
-          exitBeforeEnter={true}
-          // Fires when all exiting nodes have completed animating out
-          onExitComplete={() => null}
+        <motion.div
+          onClick={(e) => e.stopPropagation()}
+          variants={dropIn}
+          initial="hidden"
+          animate="visible"
+          exit="exit"
+          className="fixed top-0 left-0 right-0 bg-primary p-6 z-30 flex flex-col space-y-4"
         >
-          <motion.div
-            onClick={(e) => e.stopPropagation()}
-            variants={dropIn}
-            initial="hidden"
-            animate="visible"
-            exit="exit"
-            className="fixed top-0 left-0 right-0 bg-primary p-6 z-30 flex flex-col space-y-4"
-          >
-            <div className="flex justify-between items-center space-x-4 w-full lg:w-4/5 m-auto">
-              <input
-                type="text"
-                className="p-2 bg-transparent text-white border-b-2 border-white focus:outline-none placeholder-white flex-grow"
-                placeholder="Type to search blog..."
-              />
-              <CloseButton onclick={toogleSearch} />
+          <div className="flex justify-between items-center space-x-4 w-full lg:w-4/5 m-auto">
+            <input
+              type="text"
+              className="p-2 bg-transparent text-white border-b-2 border-white focus:outline-none placeholder-white flex-grow"
+              placeholder="Type to search blog..."
+            />
+            <CloseButton onclick={toogleSearch} />
+          </div>
+          <div className=" w-full lg:w-4/5 m-auto flex flex-col space-y-4">
+            <p className="p__default">See posts by tags</p>
+            <div className="flex space-x-2">
+              <Tag text={`Python`} />
+              <Tag text={`HTML`} />
             </div>
-            <div className=" w-full lg:w-4/5 m-auto flex flex-col space-y-4">
-              <p className="p__default">See posts by tags</p>
-              <div className="flex space-x-2">
-                <Tag text={`Python`} />
-                <Tag text={`HTML`} />
-              </div>
-              <p className="p__default">See posts by categories</p>
-              <div className="flex space-x-2">
-                <Tag text={`Frontend`} />
-                <Tag text={`Backend`} />
-              </div>
+            <p className="p__default">See posts by categories</p>
+            <div className="flex space-x-2">
+              <Tag text={`Frontend`} />
+              <Tag text={`Backend`} />
             </div>
-          </motion.div>
-        </AnimatePresence>
+          </div>
+        </motion.div>
       )}
       <SkipToMain />
     </>
