c7e6f8db5ee99cb31399b6bcdb9827e5a0c1602e
Kristijan007v <84925309+Kristijan007v@users.noreply.github.com>
2022-04-12 22:05:00 +0200

    Additional customatization to the 404 page

--- pages/404.js
+++ pages/404.js
@@ -8,8 +8,8 @@ export default function Custom404() {
   return (
     <>
       <Navigation />
-      <div className="text-center mt-12 m-6 flex flex-col space-y-6">
-        <p className="text-grayInfo font-extrabold text-8xl">404</p>
+      <div className="text-center mt-12 mb-16 m-auto flex flex-col space-y-6 w-5/6 md:w-3/4 lg:w-2/4 xl:w-2/5">
+        <p className="text-graySpecial font-extrabold text-8xl">404</p>
         <h1 className="heading__center">You have found a secret place.</h1>
         <p className="p__post">
           Unfortunately, this is only a 404 page. You may have mistyped the
